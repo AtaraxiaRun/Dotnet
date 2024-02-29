f6cc26f240f7ca1a961ac4d8cb8b6ec1f001b0f9
Wangrui <12988775182@qq.com>
2024-02-29 16:59:49 +0800

    基础的自定义的IOC,AspNetCore.IOC

---
+++ AspNet.IOC/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore.IOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddTransient<ILog, Log>();
+            var app = builder.Build();
+
+            var log = app.Services.GetService<ILog>();
+            log.WriteLog("张三");
+
+        }
+    }
+
+    public interface ILog
+    {
+        void WriteLog(string Message);
+    }
+
+    public class Log : ILog
+    {
+        private readonly ILogger<Log> _logger;
+        public Log(ILogger<Log> logger)
+        {
+            _logger = logger;
+        }
+        public void WriteLog(string message)
+        {
+            _logger.LogInformation(message);
+        }
+    }
+}
--- AspNet.IOC/obj/Debug/net8.0/AspNetCore.IOC.AssemblyInfo.cs
+++ AspNet.IOC/obj/Debug/net8.0/AspNetCore.IOC.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCore.Api.MiniApi")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCore.IOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("AspNetCore.Api.MiniApi")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AspNetCore.Api.MiniApi")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bef77243d79bb9c8dbed2157037f7c65e1fcf7")]
+[assembly: System.Reflection.AssemblyProductAttribute("AspNetCore.IOC")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AspNetCore.IOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
